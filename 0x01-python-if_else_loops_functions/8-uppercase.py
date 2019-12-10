6e52536d056c1aef7c4b652f3221ce5387a67b5b
luismedinaeng <malf95@hotmail.com>
2019-12-10 17:35:33 -0500

    [Update]Adding "\n" to string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        s = "\n" if i == len(str) - 1 else ""
-        i = str[i]
+    for i in str + "\n":
         to_low = ord('a') - ord('A')
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            print("{}".format(chr(ord(i) - to_low)), end=s)
+            print("{}".format(chr(ord(i) - to_low)), end="")
         else:
-            print("{}".format(chr(ord(i))), end=s)
+            print("{}".format(chr(ord(i))), end="")
