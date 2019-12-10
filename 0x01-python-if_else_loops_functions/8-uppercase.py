33d2cdf9273e4ad8f395322c92a90f8bf714e5e1
luismedinaeng <malf95@hotmail.com>
2019-12-10 17:23:54 -0500

    [Update]Run over string with chars instead of indexes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(0, len(str)):
+    for i in range(len(str)):
         s = "\n" if i == len(str) - 1 else ""
-        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-            print("{}".format(chr(ord(str[i]) - (ord('a') - ord('A')))), end=s)
+        i = str[i]
+        to_low = ord('a') - ord('A')
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            print("{}".format(chr(ord(i) - to_low)), end=s)
         else:
-            print("{}".format(chr(ord(str[i]))), end=s)
+            print("{}".format(chr(ord(i))), end=s)
