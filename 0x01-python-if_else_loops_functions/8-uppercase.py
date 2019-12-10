56eaafffbbbc1d6311d540e5f5b273c07ab0d121
luismedinaeng <malf95@hotmail.com>
2019-12-10 17:14:32 -0500

    [Update]Adding format to print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in range(0, len(str)):
         s = "\n" if i == len(str) - 1 else ""
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-            print(chr(ord(str[i]) - (ord('a') - ord('A'))), end=s)
+            print("{}".format(chr(ord(str[i]) - (ord('a') - ord('A')))), end=s)
         else:
-            print(chr(ord(str[i])), end=s)
+            print("{}".format(chr(ord(str[i]))), end=s)
