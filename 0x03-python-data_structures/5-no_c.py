e168c86272bc77903009d0ed635eacacacdb065a
luismedinaeng <malf95@hotmail.com>
2019-12-17 18:44:59 -0500

    [Fix]Styling at files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
     cpstr = ""
     for i in my_string:
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         while len(tuple_a) < 2:
