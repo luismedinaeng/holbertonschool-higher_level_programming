1136179a95184fa9383ca27fbc929067e8a23568
luismedinaeng <malf95@hotmail.com>
2019-12-19 16:25:52 -0500

    [Update]Change the iteration over the list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,4 +2,4 @@
 def uniq_add(my_list=[]):
     b = list(my_list)
     b.sort()
-    return sum([b[i] if b[i] != b[i-1] else 0 for i in range(len(b))])
+    return sum([b[i] for i in range(len(b)) if b[i] != b[i - 1] or i == 0])
