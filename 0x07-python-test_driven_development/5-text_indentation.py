8e68ea126fcf71702f79be3f3c7ef4149925617d
luismedinaeng <malf95@hotmail.com>
2020-01-13 23:59:07 -0500

    [Update]Delete the text printing

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,7 +2,6 @@
 def text_indentation(text):
     if type(text) != str:
         raise TypeError("text must be a string")
-    print(text)
     for i in text:
         if i in (".", "?", ":"):
             print("{:s}".format(i), end="\n\n")
