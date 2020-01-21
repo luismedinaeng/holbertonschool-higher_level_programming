7ecbb39129d2b81ed2a9b09ee19945a8250e02a7
luismedinaeng <malf95@hotmail.com>
2020-01-21 18:34:49 -0500

    [Update]Change the dumps function for dump

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def save_to_json_file(my_obj, filename):
     import json
-    json_rep = json.dumps(my_obj)
-    with open(filename, mode="w", encoding="utf-8") as a_file:
-        n_byt = a_file.write(json_rep)
+    json_rep = json.dump(my_obj, filename)
