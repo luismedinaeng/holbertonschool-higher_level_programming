0535bc7f853d74f96ea55341a93cb00c4b73867b
luismedinaeng <malf95@hotmail.com>
2020-01-21 18:27:16 -0500

    [Update]Main guard to the script

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,14 +5,16 @@ import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if os.path.exists("add_item.json"):
-    obj_json = load_from_json_file("add_item.json")
-else:
-    obj_json = []
+if __name__ == "__main__":
 
-argc = len(sys.argv)
+    if os.path.exists("add_item.json"):
+        obj_json = load_from_json_file("add_item.json")
+    else:
+        obj_json = []
 
-for i in range(1, argc):
-    obj_json.append(sys.argv[i])
+        argc = len(sys.argv)
 
-save_to_json_file(obj_json, "add_item.json")
+        for i in range(1, argc):
+            obj_json.append(sys.argv[i])
+
+        save_to_json_file(obj_json, "add_item.json")
