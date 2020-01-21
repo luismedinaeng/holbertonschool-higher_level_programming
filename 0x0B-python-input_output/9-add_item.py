52eaf936453f4990efeb6cc85a60eafc33e8d90c
luismedinaeng <malf95@hotmail.com>
2020-01-21 18:39:19 -0500

    [Update]Change the conditional for try/except statement

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 import sys
-import os.path
+import json
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
 
-    if os.path.exists("add_item.json"):
+    try:
         obj_json = load_from_json_file("add_item.json")
-    else:
+    except:
         obj_json = []
 
-        argc = len(sys.argv)
+    argc = len(sys.argv)
+    for i in range(1, argc):
+        obj_json.append(sys.argv[i])
 
-        for i in range(1, argc):
-            obj_json.append(sys.argv[i])
-
-        save_to_json_file(obj_json, "add_item.json")
+    save_to_json_file(obj_json, "add_item.json")
