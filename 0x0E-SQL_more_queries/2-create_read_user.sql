de80f15f7e524125b95dd131690f419c11f53373
luismedinaeng <malf95@hotmail.com>
2020-03-03 23:14:41 +0000

    [Update]Use the created database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,7 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 -- Set password for user
 SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 -- set privileges for user
+USE `hbtn_02_d`;
 GRANT SELECT ON `hbtn_02_d`.* TO 'user_0d_2'@'localhost';
 -- Update privileges
 FLUSH PRIVILEGES;
