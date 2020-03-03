17efe7b7f22eaba47b4d9311317e32456eba51b6
luismedinaeng <malf95@hotmail.com>
2020-03-03 23:21:35 +0000

    [Fix]Comments at beginning of the file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates the database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates user and set privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON `hbtn_02_d`.* TO 'user_0d_2'@'localhost'
       IDENTIFIED BY 'user_0d_2_pwd';
