fb7affa8e5abbfe3351d66ebfa1fe04b96947948
luismedinaeng <malf95@hotmail.com>
2020-03-03 23:17:58 +0000

    [Update]Change the way to create the user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,7 @@
 -- Creates the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Create user if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
--- Set password for user
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
--- set privileges for user
-USE `hbtn_02_d`;
-GRANT SELECT ON `hbtn_02_d`.* TO 'user_0d_2'@'localhost';
+-- Creates user and set privileges
+GRANT SELECT ON `hbtn_02_d`.* TO 'user_0d_2'@'localhost'
+      IDENTIFIED BY 'user_0d_2_pwd';
 -- Update privileges
 FLUSH PRIVILEGES;
