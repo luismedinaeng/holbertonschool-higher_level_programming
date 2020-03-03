e0fd3168c2b46b1157b0351a637d921b3576fe86
luismedinaeng <malf95@hotmail.com>
2020-03-03 23:25:46 +0000

    [Fix]Typo on the database name

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,10 @@
 -- Creates the database hbtn_0d_2
--- Creates user and set privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON `hbtn_02_d`.* TO 'user_0d_2'@'localhost'
-      IDENTIFIED BY 'user_0d_2_pwd';
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+-- Set password for user
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+-- set privileges for user
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+-- Update privileges
+FLUSH PRIVILEGES;
