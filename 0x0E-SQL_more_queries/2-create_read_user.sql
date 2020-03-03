03fe1825d10b2124fa6111d641ed72297e3cc2ef
luismedinaeng <malf95@hotmail.com>
2020-03-03 23:19:35 +0000

    [Delete]Update privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,3 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates user and set privileges
 GRANT SELECT ON `hbtn_02_d`.* TO 'user_0d_2'@'localhost'
       IDENTIFIED BY 'user_0d_2_pwd';
--- Update privileges
-FLUSH PRIVILEGES;
