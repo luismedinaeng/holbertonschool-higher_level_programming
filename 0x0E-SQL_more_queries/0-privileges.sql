bfd1d43ef9b2d2cbc4430baa9c167a1827f6e829
luismedinaeng <malf95@hotmail.com>
2020-03-03 22:48:04 +0000

    [Fix]Add FOR Statement

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- List all privileges of the MySQL users user_0d_1(2)
-SHOW GRANTS user_0d_1;
-SHOW GRANTS user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
