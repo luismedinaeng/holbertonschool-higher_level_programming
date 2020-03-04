f20885a9281aabf39484488dd1582e27d2a2a3d7
luismedinaeng <malf95@hotmail.com>
2020-03-04 02:16:21 +0000

    [Fix]Uppercase keywords

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all genres fron hbtn_0d_tvshows
 -- And display the number of shows to each
-SELECT name as genre, COUNT(show_id) AS number_of_shows
+SELECT name AS genre, COUNT(show_id) AS number_of_shows
 FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
