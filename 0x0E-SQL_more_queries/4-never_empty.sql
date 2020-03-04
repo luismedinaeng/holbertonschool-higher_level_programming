a581d0b60042c60b0312a6b13ad80bbe818ac77d
luismedinaeng <malf95@hotmail.com>
2020-03-04 00:30:54 +0000

    [Add]Complete Task 4 - 7

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- Creates the table id_not_null
+-- With default id value = 1 and not null
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1 NOT NULL,
+	      	     	    		 name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Creates the table states
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY AUTO_INCREMENT,
+       	     	    	   	   state_id INT NOT NULL,
+				   name VARCHAR(256),
+				   FOREIGN KEY(state_id) REFERENCES states(id));
