958f5cec2d95d7146cbecbc38b5941721ed0650a
luismedinaeng <malf95@hotmail.com>
2020-03-28 17:52:25 -0500

    [Update]SQL collation for strings

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 ''' Script that lists states starting with 'N' from database hbtn_0e_0_usa'''
 
+import sys
+import MySQLdb
+
 
 def getStatesStartingN(cursor):
     ''' Get the states of a database that starts with 'N'
     The cursor that would be used is inserted by param'''
-    strsql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+    strsql = "SELECT * FROM states WHERE name COLLATE latin1_general_cs LIKE 'N%' ORDER BY id"
     cursor.execute(strsql)
     states = cursor.fetchall()
     for state in states:
         print(state)
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
