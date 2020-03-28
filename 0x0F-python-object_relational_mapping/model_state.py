5ae5d128e0135cc281b4c2d98c327e525f5dac53
luismedinaeng <malf95@hotmail.com>
2020-03-28 16:52:47 -0500

    [Update]For back-relationship with City objects

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
-''' Module that has the definition of an State instance
-'''
+#!/usr/bin/python3
+''' Module that has the definition of an State instance'''
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,3 +14,5 @@ class State(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state")
