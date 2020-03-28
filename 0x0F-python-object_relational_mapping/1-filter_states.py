d8377904c747421b311d11f024ec9376ec66d5c4
luismedinaeng <malf95@hotmail.com>
2020-03-28 17:01:30 -0500

    [Update]Delete args verification

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,9 +15,6 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    if len(sys.argv) != 4:
-        print("Incorrect number of args")
-
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
