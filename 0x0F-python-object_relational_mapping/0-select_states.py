74b28280c01588d0c5d4dfba2a32d41d8313615f
luismedinaeng <malf95@hotmail.com>
2020-03-28 10:41:59 -0500

    [Update]Write the script in a function way

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 ''' Script that lists all states from the database hbtn_0e_0_usa'''
 
+
+def getStates(cursor):
+    ''' Get the states of a database
+    The cursor that would be used is inserted by param'''
+    strsql = "SELECT * FROM states ORDER BY id"
+    cursor.execute(strsql)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -8,13 +18,14 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Incorrect number of args")
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+    getStates(cur)
 
     cur.close()
     db.close()
