d47e331e4c516d2175bc3935ff7c58212006143e
luismedinaeng <malf95@hotmail.com>
2020-03-28 20:23:14 -0500

    [Update]Code reorganization

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,23 +2,20 @@
 ''' Script that lists states that matches the argument
 from the database hbtn_0e_0_usa with take care of SQL Injection'''
 
+import MySQLdb
+import sys
+
 
 def getStates(cursor, state_name):
     ''' Get the states of a database that matches with the param
     The cursor that would be used is inserted by param'''
-    strsql = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    strsql = "SELECT * FROM states WHERE name=%s ORDER BY id"
     cursor.execute(strsql, (state_name, ))
     states = cursor.fetchall()
     for state in states:
         print(state)
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    if len(sys.argv) != 5:
-        print("Incorrect number of args")
-
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
