c170ced36a9f798d50d548e3df4d5451171caec9
luismedinaeng <malf95@hotmail.com>
2020-04-11 20:14:04 -0500

    [Update]Follow redirections

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Sends a GET request to the URL inserted as argument, and displays the body of the response
-STATUS="$(curl -X GET -sI "$1" | grep "HTTP" | cut -d" " -f2)"
+STATUS="$(curl -X GET -sIL "$1" | grep "^HTTP/1\.. 200" -c)"
 
-if [ "$STATUS" -eq 200 ];
+if [ "$STATUS" -eq 1 ];
 then
-		curl -X GET "$1"
+    curl -X GET "$1" -L
 fi
