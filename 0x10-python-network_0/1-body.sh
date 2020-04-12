1400dc22cba2f07df86b925476935beb0dadc14d
luismedinaeng <malf95@hotmail.com>
2020-04-11 20:22:27 -0500

    [Update]Just using curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL inserted as argument, and displays the body of the response
-STATUS="$(curl -w "%{http_code}" -sL "$1" -o /dev/null)"
-
-if [ "$STATUS" -eq 1 ];
-then
-    curl -X GET "$1" -L
-fi
+curl -sL "$1" -X GET
