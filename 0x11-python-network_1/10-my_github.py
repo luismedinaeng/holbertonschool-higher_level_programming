3661e99bbcda268eb31050c18afecc6eb8f6f77c
luismedinaeng <malf95@hotmail.com>
2020-04-16 18:46:25 -0500

    [Add]Complete Task 9 - Github API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+takes your Github credentials (username and password) and
+uses the Github API to display your id
+'''
+import sys
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
+    if response.status_code == 200:
+        print(response.json()['id'])
+    else:
+        print('None')
