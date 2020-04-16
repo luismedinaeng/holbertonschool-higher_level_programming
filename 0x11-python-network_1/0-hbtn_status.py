755dccdebd19bbe72f82b49ae0be8d6181799ada
luismedinaeng <malf95@hotmail.com>
2020-04-16 15:18:05 -0500

    [Fix]Tabulations at print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,6 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         content = response.read()
-        strp = """Body response:
-        \t- type: {}
-        \t- content: {}
-        \t- utf8 content: {}
-        """
+        strp = "Body response:\n\t- type: {}"
+        strp = strp + "\n\t- content: {}\n\t- utf8 content: {}"
         print(strp.format(type(content), content, content.decode("utf-8")))
