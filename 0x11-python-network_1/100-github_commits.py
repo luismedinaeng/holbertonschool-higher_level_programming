052ebb4ab24ed3aa059b905f5d5030eef06e38c0
luismedinaeng <malf95@hotmail.com>
2020-04-16 19:26:10 -0500

    [Update]Change the order of the repo and owner args

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits"
-    response = requests.get(url.format(sys.argv[1], sys.argv[2]))
+    response = requests.get(url.format(sys.argv[2], sys.argv[1]))
     a = 0
     for i in response.json():
         print("{}: {}".format(i['sha'], i['commit']['author']['name']))
