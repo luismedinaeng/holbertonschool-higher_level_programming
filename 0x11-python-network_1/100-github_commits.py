64854ba15f5a01af6f08ab3f4cf2017871bea2a3
luismedinaeng <malf95@hotmail.com>
2020-04-16 19:24:00 -0500

    [Add]Complete Task 10 - Github commmits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Lists teh last 10 commits on a repository
+'''
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"
+    response = requests.get(url.format(sys.argv[1], sys.argv[2]))
+    a = 0
+    for i in response.json():
+        print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+        a = a + 1
+        if a == 10:
+            break
