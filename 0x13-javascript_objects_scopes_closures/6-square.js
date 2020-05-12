b0297798833c876a0257f2b1b67a7cd41de63f47
luismedinaeng <malf95@hotmail.com>
2020-05-11 22:39:54 -0500

    [Update]Importing class square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,41 +1,7 @@
 #!/usr/bin/node
-const Rectangle = class {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const SquareBase = require('./5-square');
 
-  print () {
-    let recPrint = '';
-    for (let i = 0; i < this.width; i++) {
-      recPrint += 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(recPrint);
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-};
-module.exports = Rectangle;
-
-const Square1 = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-};
-module.exports = Square1;
-
-const Square2 = class Square extends Square1 {
+const Square = class Square extends SquareBase {
   charPrint (c) {
     c = c || 'X';
     let recPrint = '';
@@ -47,4 +13,4 @@ const Square2 = class Square extends Square1 {
     }
   }
 };
-module.exports = Square2;
+module.exports = Square;
