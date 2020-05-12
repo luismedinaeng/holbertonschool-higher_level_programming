bb36af50eaa5ccd8d7356f83f9e6a8bcb1537dca
luismedinaeng <malf95@hotmail.com>
2020-05-11 23:58:49 -0500

    [Update]using index of map insted of searching index object

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-console.log(list.map(function (x) {
-  return list.indexOf(x) * x;
+console.log(list.map(function (x, i) {
+  return i * x;
 }));
