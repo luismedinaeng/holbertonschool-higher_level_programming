4938aaa2781622fc69623a43d177c44adbad2f48
luismedinaeng <malf95@hotmail.com>
2020-05-11 22:19:50 -0500

    [Add]complete Task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const Rectangle = class {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let recPrint = '';
+    for (let i = 0; i < this.width; i++) {
+      recPrint += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(recPrint);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+module.exports = Rectangle;
+
+const Square = class extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
+module.exports = Square;
