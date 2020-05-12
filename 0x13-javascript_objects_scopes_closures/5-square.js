ae95e57faf5604344510bcb2a0e6545a4c279e79
luismedinaeng <malf95@hotmail.com>
2020-05-11 22:36:43 -0500

    [Update]Import class Rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,32 +1,5 @@
 #!/usr/bin/node
-const Rectangle = class {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let recPrint = '';
-    for (let i = 0; i < this.width; i++) {
-      recPrint += 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(recPrint);
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-};
-module.exports = Rectangle;
+const Rectangle = require('./4-rectangle.js');
 
 const Square = class extends Rectangle {
   constructor (size) {
